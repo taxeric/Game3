574917db4173d9871504f40e401a42679b6c1d14
幻弦让叶 <51876689+taxeric@users.noreply.github.com>
2024-09-21 21:19:53 +0800

    first commit

---
+++ src/main/kotlin/com/lanier/game/plugins/Serialization.kt
@@ -0,0 +1,32 @@
+package com.lanier.game.plugins
+
+import com.fasterxml.jackson.databind.*
+import io.ktor.serialization.gson.*
+import io.ktor.serialization.jackson.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+        gson {
+        }
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+    routing {
+        get("/json/kotlinx-serialization") {
+            call.respond(mapOf("hello" to "world"))
+        }
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+        get("/json/jackson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
