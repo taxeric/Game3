86599ebd038000a3f8de84c3c84abe900f0ea893
幻弦让叶 <51876689+taxeric@users.noreply.github.com>
2024-09-22 19:48:27 +0800

    opt: 切换路由 & 请求方式

--- src/main/kotlin/com/lanier/game/feature/user/InstallUserModule.kt
+++ src/main/kotlin/com/lanier/game/feature/user/InstallUserModule.kt
@@ -4,9 +4,14 @@ import com.lanier.game.model.UserModel
 import com.lanier.game.model.respError
 import com.lanier.game.model.respSuccess
 import io.ktor.server.application.Application
+import io.ktor.server.request.receiveText
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
 /**
  * Created by 幻弦让叶
@@ -17,22 +22,26 @@ fun Application.installUserModule() {
     val dao: UserDao = UserDaoImpl()
 
     routing {
-        get("/get-user") {
-            val account = call.request.queryParameters["account"]
-            val password = call.request.queryParameters["password"]
+        post("/login") {
+
+            val jsonString = call.receiveText()
+            val json = Json.parseToJsonElement(jsonString).jsonObject
+            val account = json["account"]?.jsonPrimitive?.content
+            val password = json["password"]?.jsonPrimitive?.content
+
             if (account.isNullOrBlank()) {
                 call.respond(respError<UserModel>(message = "Missing account parameter"))
-                return@get
+                return@post
             }
             if (password.isNullOrBlank()) {
                 call.respond(respError<UserModel>(message = "Missing password parameter"))
-                return@get
+                return@post
             }
 
             val user = dao.getUser(account = account, password = password)
             if (user == null) {
                 call.respond(respError<UserModel>(message = "User not found"))
-                return@get
+                return@post
             }
 
             call.respond(respSuccess<UserModel>(data = user))
